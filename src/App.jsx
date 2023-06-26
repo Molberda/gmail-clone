2c6de3d51761a7098ae9d67b80b68ea5f30d691d
David Moller <davidmollera@hotmail.com>
2023-06-26 18:13:26 -0500

    cleaned up the redux template

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CSS/App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className='title'>lets build gmail</h1>
+    </div>
+  );
+}
+
+export default App;
