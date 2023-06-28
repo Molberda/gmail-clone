ade810a001dd9f7c3bb0c3279c2122570fa0a574
David Moller <davidmollera@hotmail.com>
2023-06-27 23:39:54 -0500

    wrapped menu icon in an icon btn and used the imgg tag to use the gmail logo img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import "../CSS/Header.css";
 import { Menu } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 
 const Header = () => {
   return (
     <header>
       <div className="header__left">
-        <Menu />
+        <IconButton>
+          <Menu />
+        </IconButton>
+        <img
+          src="https://cdn.vox-cdn.com/thumbor/jJ_w_lWMMvGKoaLp_zaEXJpyZ9c=/0x0:1320x880/1400x788/filters:focal(660x440:661x441)/cdn.vox-cdn.com/uploads/chorus_asset/file/21939811/newgmaillogo.jpg"
+          alt=""
+        />
       </div>
 
       <div className="header__middle"></div>
