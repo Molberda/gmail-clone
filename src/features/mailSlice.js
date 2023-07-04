ac788c0e45dad3f4bce38c8508babee8cd26ff35
David Moller <davidmollera@hotmail.com>
2023-07-04 16:23:28 -0500

    added a selectMail reducer to the mailSLice js file

--- src/features/mailSlice.js
+++ src/features/mailSlice.js
@@ -1,32 +1,15 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-// import { fetchCount } from './counter/counterAPI';
-
-const initialState = {
-  value: 0,
-  status: 'idle',
-};
-
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched. Thunks are
-// typically used to make async requests.
-// export const incrementAsync = createAsyncThunk(
-//   'counter/fetchCount',
-//   async (amount) => {
-//     const response = await fetchCount(amount);
-//     // The value we return becomes the `fulfilled` action payload
-//     return response.data;
-//   }
-// );
+import { createSlice } from "@reduxjs/toolkit";
 
 export const mailSlice = createSlice({
-  name: 'mail',
+  name: "mail",
   initialState: {
-    sendMessageIsOpen: false
+    selectedMail: null,
+    sendMessageIsOpen: false,
   },
-  // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
+    selectMail: (state, action) => {
+      state.selectedMail = action.payload;
+    },
     openSendMessage: (state) => {
       state.sendMessageIsOpen = true;
     },
@@ -36,20 +19,11 @@ export const mailSlice = createSlice({
   },
 });
 
-export const { openSendMessage, closeSendMessage } = mailSlice.actions;
+export const { selectMail, openSendMessage, closeSendMessage } =
+  mailSlice.actions;
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
+export const selectOpenMail = (state) => state.mail.selectedMail;
+
 export const selectSendMessageIsOpen = (state) => state.mail.sendMessageIsOpen;
-
-// // We can also write thunks by hand, which may contain both sync and async logic.
-// // Here's an example of conditionally dispatching actions based on current state.
-// export const incrementIfOdd = (amount) => (dispatch, getState) => {
-//   const currentValue = selectmail(getState());
-//   if (currentValue % 2 === 1) {
-//     dispatch(incrementByAmount(amount));
-//   }
-// };
 
 export default mailSlice.reducer;
