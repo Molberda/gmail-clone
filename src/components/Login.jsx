0b078286d2d1124c1388fe494b4c0e771254f9f9
David Moller <davidmollera@hotmail.com>
2023-07-07 13:13:00 -0500

    createed the login function on the login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import "../CSS/Login.css";
 import { Button } from "@mui/material";
-import { auth } from "../firebase";
+import { auth, provider } from "../firebase";
+import { useDispatch } from "react-redux";
+import { login } from "../features/userSlice";
 
 const Login = () => {
-    const signIn = () => {
-        auth
-    }
+
+    const dispatch = useDispatch()
+  const signIn = () => {
+    auth
+    .signInWithPopup(provider)
+    .then(({ user }) => {
+      dispatch(login({
+          displayName: user.displayName,
+          email: user.email,
+          photoUrl: user.photoURL,
+        }));
+    });
+    
+  }
   return (
     <div className="login">
       <div className="login__container">
@@ -16,10 +29,12 @@ const Login = () => {
             alt=""
           />
         </figure>
-        <Button>Login</Button>
+        <Button onClick={() => signIn()}>Login</Button>
       </div>
     </div>
   );
 };
+
+// https://youtu.be/b7nrXjS6Dqs?t=14874
 
 export default Login;
