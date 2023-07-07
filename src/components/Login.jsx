10c5878a6ec072d3890eb46b9ce6ee8bdf55848e
David Moller <davidmollera@hotmail.com>
2023-07-07 12:26:46 -0500

    added some initial html and css for the login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import '../CSS/Login.css'
+import React from "react";
+import "../CSS/Login.css";
 
 const Login = () => {
-    return (
-        <div className='login'>
-            test
-        </div>
-    );
-}
+  return (
+    <div className="login">
+      <div className="login__container">
+        <figure>
+          <img
+            src="https://cdn.vox-cdn.com/thumbor/jJ_w_lWMMvGKoaLp_zaEXJpyZ9c=/0x0:1320x880/1400x788/filters:focal(660x440:661x441)/cdn.vox-cdn.com/uploads/chorus_asset/file/21939811/newgmaillogo.jpg"
+            alt=""
+          />
+        </figure>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
